0fc4555b266f0a66f0a98e20eb89b017e53f237d
Jonathan Yu <jonyu02@gmail.com>
2021-06-09 17:57:10 -0400

    commit tree 5e7ca4196397d50852d25ab6b02cf4969370096a

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,15 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
   end
 
   def cobble
